5ab46791f8048d4746245825afb8c7d0d2469aa6
KlinVid <stas.nik12@gmail.com>
2023-04-30 22:45:59 +0300

    фикс стилей, добавление авто слайдера

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,45 +1,53 @@
- const next = document.querySelector('#next');
- const previous = document.querySelector('#previous');
- const autoSlider = document.querySelector('.slider');
- 
- /* Устанавливаем стартовый индекс слайда по умолчанию: */
- let slideIndex = 1;
- /* Вызываем функцию, которая реализована ниже: */
- showSlides(slideIndex);
+//slider
+const next = document.querySelector("#next");
+const previous = document.querySelector("#previous");
+const autoSlider = document.querySelector(".slider");
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
- /* Увеличиваем индекс на 1 — показываем следующий слайд: */
- next.addEventListener('click', () => {
-     showSlides(slideIndex += 1);
- });
+next.addEventListener("click", () => {
+  showSlides((slideIndex += 1));
+});
 
- /* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
-previous.addEventListener('click', () => {
-     showSlides(slideIndex -= 1);
- });
+previous.addEventListener("click", () => {
+  showSlides((slideIndex -= 1));
+});
 
- /* Устанавливаем текущий слайд: */
- function currentSlide(n) {
-     showSlides(slideIndex = n);
- }
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
- /* Функция перелистывания: */
- function showSlides(n) {
-     /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-     let slides = document.getElementsByClassName("slider-item");
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slider-item");
 
-     /* Проверяем количество слайдов: */
-     if (n > slides.length) {
-         slideIndex = 1
-     }
-     if (n < 1) {
-         slideIndex = slides.length
-     }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-     /* Проходим по каждому слайду в цикле for: */
-     for (let slide of slides) {
-         slide.style.display = "none";
-     }
-     /* Делаем элемент блочным: */
-     slides[slideIndex - 1].style.display = "block";
- }
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+//auto-slider
+let autoIndex = 0;
+autoSlides();
+
+function autoSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slider-item");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  autoIndex++;
+  if (autoIndex > slides.length) {
+    autoIndex = 1;
+  }
+  slides[autoIndex - 1].style.display = "block";
+  setTimeout(autoSlides, 5000);
+}
