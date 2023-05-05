3f675b998905ff732f887110d3741aad3d2e2ce6
StasNik <stas.nik12@gmail.com>
2023-05-05 11:02:22 +0300

    tabs

---
+++ assets/scripts/news.js
@@ -0,0 +1,29 @@
+const tabsBtn   = document.querySelectorAll(".news-tabs-btn");
+const newsItems = document.querySelectorAll(".news-tabs-item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if( ! currentBtn.classList.contains('active') ) {
+            tabsBtn.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            newsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+        }
+    });
+}
+
+document.querySelector('.news-tabs-btn').click();
+
+
