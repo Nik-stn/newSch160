7694e60523a2f4dbb530f0f58f6ab9802c7728d4
StasNik <stas.nik12@gmail.com>
2023-05-12 11:40:44 +0300

    оптимизация скрипта баннер

--- assets/scripts/banners.js
+++ assets/scripts/banners.js
@@ -1,42 +1,62 @@
-(function() {
-    let banners = document.querySelectorAll('.banners');
-    
-    [].forEach.call(banners, function(carousel) {
+(function () {
+  let banners = document.querySelectorAll('.banners');
+
+  [].forEach.call(banners, function (carousel) {
+    carouselize(carousel);
+  });
+
+  window.addEventListener('resize', function () {
+    [].forEach.call(banners, function (carousel) {
       carouselize(carousel);
     });
-    
-  })();
+  });
+
+})();
+
+function carouselize(carousel) {
+  let bannersInner = carousel.querySelector('.banners-inner');
+  let bannerLogos = carousel.querySelectorAll('.banner-logo');
+  let logoAmount = bannerLogos.length;
+  let logoWidth = 0;
+  let logoAmountVisible = 0;
+  let clientWidth = document.documentElement.clientWidth;
+
+  [].forEach.call(bannerLogos, function (bannerLogo) {
+    logoWidth += bannerLogo.offsetWidth;
+  });
+
+  console.log(logoWidth)
+  logoAmountVisible = Math.floor(clientWidth / (logoWidth / bannerLogos.length));
+  console.log(logoAmountVisible)
   
-  function carouselize(carousel) {
-    let bannersInner = carousel.querySelector('.banners-inner');
-    let bannerLogo = carousel.querySelectorAll('.banner-logo');
-    let logoWidth = 0;
-    let logoSteps = 0;
-    let logoAmount = 0;
-    let logoAmountVisible = 9;
-    let btnPrev = carousel.querySelector('.banners-control.previous');
-    let btnNext = carousel.querySelector('.banners-control.next');
-  
-    [].forEach.call(bannerLogo, function() {
-      logoAmount++;
-      logoWidth += 250;
-      bannersInner.style.width = logoWidth+"px";
-    });
-  
-    btnNext.onclick = function() {
-      if(logoSteps < logoAmount-logoAmountVisible) {
-        logoSteps++;
-        moveLogo();
-      }
-    }
-    btnPrev.onclick = function() {
-      if(logoSteps > 0) {
-        logoSteps--;
-        moveLogo();
-      }
-    }
-    
-    function moveLogo() {
-      bannersInner.style.transform = "translateX(-"+205*logoSteps+"px)";
+  bannersInner.style.width = (logoWidth + (logoAmountVisible * 20)) + 'px';
+
+  let logoSteps = 0;
+  let btnPrev = carousel.querySelector('.banners-control.previous');
+  let btnNext = carousel.querySelector('.banners-control.next');
+
+  btnNext.onclick = function () {
+    if (logoSteps < (bannerLogos.length - logoAmountVisible)) {
+      logoSteps++;
+      moveLogo();
     }
   }
+
+  btnPrev.onclick = function () {
+    if (logoSteps > 0) {
+      logoSteps--;
+      moveLogo();
+    }
+  }
+
+  function moveLogo() {
+    let maxSteps = logoAmount - logoAmountVisible;
+    if (logoSteps < 0) {
+      logoSteps = 0;
+    } else if (logoSteps > maxSteps) {
+      logoSteps = maxSteps;
+    }
+    bannersInner.style.transform = "translateX(-" + (205 * logoSteps) + "px)";
+  }
+
+}
