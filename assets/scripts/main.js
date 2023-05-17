25d8636d7fd66f4bba9654554d30582918ae3cb6
StasNik <stas.nik12@gmail.com>
2023-05-17 12:53:21 +0300

    pos fixed header-nav

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,23 @@
-document.addEventListener('scroll', () => {
-    const scrollUp = document.querySelector('.scrooll-to-top');
-    const scrollPosition = window.scrollY;
-    const windowWidth = window.innerWidth;
-    
-    if (scrollPosition > 100 && windowWidth > 768) {
-      scrollUp.style.display = 'block';
-    } else {
-      scrollUp.style.display = 'none';
-    }
-  });
-  
-  document.querySelector('.scrooll-to-top').addEventListener('click', (event) => {
-    event.preventDefault(); 
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
-  
+window.addEventListener('scroll', () => {
+  const scrollUp = document.querySelector('.scrooll-to-top');
+  const scrollPosition = window.scrollY;
+  const windowWidth = window.innerWidth;
+  let headerFixed = document.querySelector('.header-nav');
+
+  if (scrollPosition > 100 && windowWidth > 768) {
+    scrollUp.style.display = 'block';
+  } else {
+    scrollUp.style.display = 'none';
+  }
+
+  if (scrollPosition >= 200) {
+    headerFixed.classList.add('fixed');
+  } else {
+    headerFixed.classList.remove('fixed');
+  }
+});
+
+document.querySelector('.scrooll-to-top').addEventListener('click', (event) => {
+  event.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
