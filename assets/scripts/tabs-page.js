1885b8fa8019b0e57603711ef58840dbcf0cdb90
StasNik <stas.nik12@gmail.com>
2023-06-20 10:36:24 +0300

    tabs для других страниц - начало

---
+++ assets/scripts/tabs-page.js
@@ -0,0 +1,30 @@
+const tabsBtn = document.querySelectorAll(".news-tabs-btn");
+const newsItems = document.querySelectorAll(".news-tabs-item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function () {
+        const currentBtn = item;
+        const tabId = currentBtn.getAttribute("data-tab");
+        const currentTab = document.querySelector(tabId);
+
+        if (!currentBtn.classList.contains('active')) {
+            tabsBtn.forEach(function (item) {
+                item.classList.remove('active');
+            });
+
+            newsItems.forEach(function (item) {
+                item.classList.remove('active');
+            });
+
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+
+            // Сохраняем ID активного таба в sessionStorage
+            sessionStorage.setItem('activeTab', tabId);
+            sessionStorage.setItem('activeTabBtn', currentBtn.getAttribute('id'));
+
+        }
+    });
+}
