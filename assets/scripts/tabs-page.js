d6a7f0a30f660c29e481e50074796ad4518991f9
StasNik <stas.nik12@gmail.com>
2023-06-21 11:49:15 +0300

    скрипт для вкладок

--- assets/scripts/tabs-page.js
+++ assets/scripts/tabs-page.js
@@ -1,30 +1,30 @@
-const tabsBtn = document.querySelectorAll(".news-tabs-btn");
-const newsItems = document.querySelectorAll(".news-tabs-item");
+const tabsLink = document.querySelectorAll(".tabs-link");
+const tabsItem = document.querySelectorAll(".tabs-item");
+const firstLink = document.querySelector(".tabs-link");
+const firstItem = document.querySelector(".tabs-item");
 
-tabsBtn.forEach(onTabClick);
+firstLink.classList.add('active');
+firstItem.classList.add('active');
+
+tabsLink.forEach(onTabClick);
 
 function onTabClick(item) {
     item.addEventListener("click", function () {
-        const currentBtn = item;
-        const tabId = currentBtn.getAttribute("data-tab");
+        const currentLink = item;
+        const tabId = currentLink.getAttribute("data-tab");
         const currentTab = document.querySelector(tabId);
 
-        if (!currentBtn.classList.contains('active')) {
-            tabsBtn.forEach(function (item) {
+        if (!currentLink.classList.contains('active')) {
+            tabsLink.forEach(function (item) {
                 item.classList.remove('active');
             });
 
-            newsItems.forEach(function (item) {
+            tabsItem.forEach(function (item) {
                 item.classList.remove('active');
             });
 
-            currentBtn.classList.add('active');
+            currentLink.classList.add('active');
             currentTab.classList.add('active');
-
-            // Сохраняем ID активного таба в sessionStorage
-            sessionStorage.setItem('activeTab', tabId);
-            sessionStorage.setItem('activeTabBtn', currentBtn.getAttribute('id'));
-
         }
     });
 }
