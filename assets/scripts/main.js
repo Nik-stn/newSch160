4f028a998ec38da251484ccedc21aba31c3d1f2a
StasNik <stas.nik12@gmail.com>
2023-10-10 13:05:20 +0300

    добавление modal img

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,3 +21,26 @@ document.querySelector('.scrooll-to-top').addEventListener('click', (event) => {
   event.preventDefault();
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+// Modal img
+const images = document.querySelectorAll(".post_content img, .html_block img, .excerpt_content img");
+let imgSrc;
+images.forEach((img) => {
+    img.addEventListener("click", (e) => {
+        imgSrc = e.target.src;
+        imgModal(imgSrc);
+    });
+});
+
+let imgModal = (src) => {
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+    document.querySelector(".main").append(modal);
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", src);
+    newImage.onclick = () => {
+        modal.remove();
+    };
+    modal.append(newImage);
+};
+
