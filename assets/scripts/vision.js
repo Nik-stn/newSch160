4571c758cea76aa501643f0967d21f92c145a8c2
StasNik <stas.nik12@gmail.com>
2023-10-26 11:35:21 +0300

    Корректировка vision и добавлена eye-white svg

--- assets/scripts/vision.js
+++ assets/scripts/vision.js
@@ -1,17 +1,10 @@
 const img = document.querySelectorAll("#ya-site-form0 input.ya-site-form__submit, img, .sch-map,  .slider-icon, .banners-icon, .header-mobile .parent");
-
 const siteContent = document.querySelectorAll(".site-content, .popular-item, .news-sch-item, .announcement-sch-item");
-
 const buttons = document.querySelectorAll(".popular-item, .btn-header-bar, .header-nav, .news-tabs-btn, .announcement-tab-btn, .news-sch-arcive-text, .announcement-sch-arcive-text, .footer, .header-mobile .level_0");
-
 const checkbox = document.querySelectorAll(".checkbox-line span, .scrooll-to-top");
-
-const noBorder = document.querySelectorAll('.footer-flex');
-
+const noBorder = document.querySelectorAll('.footer-flex, .news-sch-item, .announcement-sch-item');
 const links = document.querySelectorAll('.header-pc .parent>a, .header-pc .parent>span, .news-tabs-btn, .news-sch-content, .announcement-sch-content, .footer, .about-text, .footer-nav .parent, .count-title, .about-text-item a, .footer-nav li a, .footer-copyright a, .footer-copyright, .header-mobile .parent>a, .header-mobile .level_1>li>a, .popular-item a, .link-header-bar, .news-sch-archive-link, .announcement-sch-archive-link');
-
 const mostSelectors = document.querySelectorAll('.popular-item, .btn-header-bar, .header-nav, .main, .header-pc .parent>a, .header-pc .parent>span, .news-tabs-btn, .announcement-tab-btn, .news-sch-content, .announcement-sch-content, .news-sch-arcive-text, .announcement-sch-arcive-text, .footer, .about-text, .footer-nav .parent, .count-title, .about-text-item a, .footer-nav li a, .footer-copyright a, .footer-copyright, .header-mobile .level_0, .header-mobile .parent>a, .header-mobile .level_1>li>a, .checkbox-line span, .scrooll-to-top');
-
 const noHover = document.querySelectorAll('.header-pc .parent>a:hover::before, .header-pc .parent>span:hover::before');
 
 function tenderEyed(event) {
@@ -24,7 +17,7 @@ function tenderEyed(event) {
     links.forEach(item => item.classList.add('linkColorSiteWhite'));
     buttons.forEach(item => item.classList.add('buttonColorSiteWhite'));
     checkbox.forEach(item => item.classList.add('checkboxColorSiteWhite'));
-    noBorder.forEach(item => item.classList.add('footerNoBorder'));
+    noBorder.forEach(item => item.classList.add('footerNoBorder', 'newsNoBorder'));
     noHover.forEach(item => item.style.backgroundColor = 'inherit');
 }
 
@@ -33,6 +26,7 @@ function normalEyed(event) {
     document.body.removeAttribute("class");
     document.getElementById("imagesYes").dispatchEvent(new Event("click"));
     document.getElementById("normal-eyed").style.display = "none";
+    document.getElementById("normal-eyed-white").style.display = "none";
     document.querySelector(".vision").style.display = "none";
     document.getElementById("tender-eyed").style.display = "inline-block";
     mostSelectors.forEach( item => item.removeAttribute("style"));
@@ -40,7 +34,7 @@ function normalEyed(event) {
     links.forEach( item => item.classList.remove("linkColorSiteBlue", "linkColorSiteWhite", "linkColorSiteBlack" ,"linkColorSiteBeige"));
     buttons.forEach( item => item.classList.remove('buttonColorSiteBlue', "buttonColorSiteWhite", "buttonColorSiteBlack", "buttonColorSiteBeige"));
     checkbox.forEach( item => item.classList.remove('checkboxColorSiteBlue', "checkboxColorSiteWhite", "checkboxColorSiteBlack", "checkboxColorSiteBeige"));
-    noBorder.forEach( item => item.classList.remove('footerNoBorder'));
+    noBorder.forEach( item => item.classList.remove('footerNoBorder', 'newsNoBorder'));
 }
 
 function updateFontSizes(changeAmount) {
@@ -51,97 +45,79 @@ function updateFontSizes(changeAmount) {
       else if (fz > 24) fz = 24;
       else fz = 12
     });
-  }
+}
 
-  function colorSiteClick(event) {
+function colorSiteClick(event) {
     event.preventDefault();
     document.body.removeAttribute("class");
     let id = this.getAttribute("id");
     document.body.classList.add(id);
     if (id === 'colorSiteBlue') {
-        links.forEach(function (item) {
+        document.getElementById("normal-eyed").style.display = "inline-block";
+        document.getElementById("normal-eyed-white").style.display = "none";
+        links.forEach( item => {
             item.classList.add('linkColorSiteBlue');
-            item.classList.remove("linkColorSiteWhite");
-            item.classList.remove("linkColorSiteBlack");
-            item.classList.remove("linkColorSiteBeige");
+            item.classList.remove("linkColorSiteWhite", "linkColorSiteBlack", "linkColorSiteBeige");
         });
-        buttons.forEach(function (item) {
+        buttons.forEach( item => {
             item.classList.add('buttonColorSiteBlue');
-            item.classList.remove("buttonColorSiteWhite");
-            item.classList.remove("buttonColorSiteBlack");
-            item.classList.remove("buttonColorSiteBeige");
+            item.classList.remove("buttonColorSiteWhite", "buttonColorSiteBlack", "buttonColorSiteBeige");
         });
-        checkbox.forEach(function (item) {
+        checkbox.forEach( item=> {
             item.classList.add('checkboxColorSiteBlue');
-            item.classList.remove("checkboxColorSiteWhite");
-            item.classList.remove("checkboxColorSiteBlack");
-            item.classList.remove("checkboxColorSiteBeige");
+            item.classList.remove("checkboxColorSiteWhite", "checkboxColorSiteBlack", "checkboxColorSiteBeige");
         });
     } else if (id === 'colorSiteBlack') {
-        links.forEach(function (item) {
+        document.getElementById("normal-eyed").style.display = "none";
+        document.getElementById("normal-eyed-white").style.display = "inline-block";    
+        links.forEach( item=> {
             item.classList.add('linkColorSiteBlack');
-            item.classList.remove("linkColorSiteWhite");
-            item.classList.remove("linkColorSiteBlue");
-            item.classList.remove("linkColorSiteBeige");
+            item.classList.remove("linkColorSiteWhite", "linkColorSiteBlue", "linkColorSiteBeige");
         });
-        buttons.forEach(function (item) {
+        buttons.forEach( item=> {
             item.classList.add('buttonColorSiteBlack');
-            item.classList.remove("buttonColorSiteWhite");
-            item.classList.remove("buttonColorSiteBlue");
-            item.classList.remove("buttonColorSiteBeige");
+            item.classList.remove("buttonColorSiteWhite", "buttonColorSiteBlue", "buttonColorSiteBeige");
         });
-        checkbox.forEach(function (item) {
+        checkbox.forEach( item=> {
             item.classList.add('checkboxColorSiteBlack');
-            item.classList.remove("checkboxColorSiteWhite");
-            item.classList.remove("checkboxColorSiteBlue");
-            item.classList.remove("checkboxColorSiteBeige");
+            item.classList.remove("checkboxColorSiteWhite", "checkboxColorSiteBlue", "checkboxColorSiteBeige");
         });
     } else if (id === 'colorSiteWhite') {
-        links.forEach(function (item) {
+        document.getElementById("normal-eyed").style.display = "inline-block";
+        document.getElementById("normal-eyed-white").style.display = "none";
+        links.forEach( item=> {
             item.classList.add('linkColorSiteWhite');
-            item.classList.remove("linkColorSiteBlack");
-            item.classList.remove("linkColorSiteBlue");
-            item.classList.remove("linkColorSiteBeige");
+            item.classList.remove("linkColorSiteBlack", "linkColorSiteBlue", "linkColorSiteBeige");
         });
-        buttons.forEach(function (item) {
+        buttons.forEach( item=> {
             item.classList.add('buttonColorSiteWhite');
-            item.classList.remove("buttonColorSiteBlack");
-            item.classList.remove("buttonColorSiteBlue");
-            item.classList.remove("buttonColorSiteBeige");
+            item.classList.remove("buttonColorSiteBlack", "buttonColorSiteBlue", "buttonColorSiteBeige");
         });
-        checkbox.forEach(function (item) {
+        checkbox.forEach( item=> {
             item.classList.add('checkboxColorSiteWhite');
-            item.classList.remove("checkboxColorSiteBlack");
-            item.classList.remove("checkboxColorSiteBlue");
-            item.classList.remove("checkboxColorSiteBeige");
+            item.classList.remove("checkboxColorSiteBlack", "checkboxColorSiteBlue", "checkboxColorSiteBeige");
         });
     } else if (id === 'colorSiteBeige') {
-        links.forEach(function (item) {
+        document.getElementById("normal-eyed").style.display = "inline-block";
+        document.getElementById("normal-eyed-white").style.display = "none";
+        links.forEach( item=> {
             item.classList.add('linkColorSiteBeige');
-            item.classList.remove("linkColorSiteBlack");
-            item.classList.remove("linkColorSiteBlue");
-            item.classList.remove("linkColorSiteWhite");
+            item.classList.remove("linkColorSiteBlack", "linkColorSiteBlue", "linkColorSiteWhite");
         });
-        buttons.forEach(function (item) {
+        buttons.forEach( item=> {
             item.classList.add('buttonColorSiteBeige');
-            item.classList.remove("buttonColorSiteBlack");
-            item.classList.remove("buttonColorSiteBlue");
-            item.classList.remove("buttonColorSiteWhite");
+            item.classList.remove("buttonColorSiteBlack", "buttonColorSiteBlue", "buttonColorSiteWhite");
         });
-        checkbox.forEach(function (item) {
+        checkbox.forEach( item=> {
             item.classList.add('checkboxColorSiteBeige');
-            item.classList.remove("checkboxColorSiteBlack");
-            item.classList.remove("checkboxColorSiteBlue");
-            item.classList.remove("checkboxColorSiteWhite");
+            item.classList.remove("checkboxColorSiteBlack", "checkboxColorSiteBlue", "checkboxColorSiteWhite");
         });
-    } else {
-        console.log('2134');
-    }
+    } else console.log('Error classlist');
 }
 
 function imagesNoClick(event) {
     event.preventDefault();
-    img.forEach(function (item) {
+    img.forEach( item => {
         item.classList.remove("imagesBlackWhite");
         item.style.display = "none";
     });
@@ -149,7 +125,7 @@ function imagesNoClick(event) {
 
 function imagesYesClick(event) {
     event.preventDefault();
-    img.forEach(function (item) {
+    img.forEach( item => {
         item.classList.remove("imagesBlackWhite");
         item.style.display = "block";
     });
@@ -157,7 +133,7 @@ function imagesYesClick(event) {
 
 function imagesBlackWhiteClick(event) {
     event.preventDefault();
-    img.forEach(function (item) {
+    img.forEach( item => {
         item.classList.add("imagesBlackWhite");
         item.style.display = "block";
     });
@@ -165,9 +141,7 @@ function imagesBlackWhiteClick(event) {
 
 function spacing() {
     const data = this.getAttribute("data-letter-spacing");
-    siteContent.forEach((item) => {
-        item.style.letterSpacing = data + 'px';
-    });
+    siteContent.forEach( item => item.style.letterSpacing = data + 'px');
 }
 
 function init() {
@@ -175,19 +149,11 @@ function init() {
     document.getElementById("normal-eyed").addEventListener("click", normalEyed);
     document.getElementById("reduceFont").addEventListener("click", () => updateFontSizes(-1));
     document.getElementById("increaseFont").addEventListener("click", () => updateFontSizes(1));
-
-    document.querySelectorAll(".colorSite").forEach(function (item) {
-        item.addEventListener("click", colorSiteClick);
-    });
-
-    document.querySelectorAll(".changeLetterSpacing").forEach((item) => {
-        item.addEventListener("click", spacing);
-    });
-
+    document.querySelectorAll(".colorSite").forEach( item => item.addEventListener("click", colorSiteClick));
+    document.querySelectorAll(".changeLetterSpacing").forEach( item => item.addEventListener("click", spacing));
     document.getElementById("imagesNo").addEventListener("click", imagesNoClick);
     document.getElementById("imagesYes").addEventListener("click", imagesYesClick);
     document.getElementById("imagesBlackWhite").addEventListener("click", imagesBlackWhiteClick);
-
 }
 
 init();
